13afda3e1b4411124cf6fa1105c402a99a1a8182
janmesh kumar <janmesh799@gmail.com>
2023-05-01 01:48:23 +0530

    changed backend service host

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5000/api/'
+// const API_URL = 'http://localhost:5000/api/'
+const API_URL = 'https://upasthit-backend.vercel.app/'
 
 //register user
 const signup = async (userData) => {
