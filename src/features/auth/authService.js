8132427234d5868af071e7c808d385b5b592db77
Janmesh Kumar <68043753+janmesh799@users.noreply.github.com>
2023-05-01 01:54:18 +0530

    Update authService.js

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const API_URL = 'http://localhost:5000/api/'
-const API_URL = 'https://upasthit-backend.vercel.app/'
+const API_URL = 'https://upasthit-backend.vercel.app/api/'
 
 //register user
 const signup = async (userData) => {
